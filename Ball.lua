0d4e018f38ea22a32011b9b57c19698ed8bdc18d
Robert Woods <robbiewoods05@gmail.com>
2016-02-02 01:51:37 +0000

    Trigonometry based collisions

--- Ball.lua
+++ Ball.lua
@@ -1,6 +1,6 @@
-local Ball = {  x, y, velX, velY, edge  = 0, 0, 0, 0, collided = false }
+local Ball = {  x, y, velX, velY, edge, xV, yV  = 0, 0, 0, 0, 0, 0, collided = false }
 
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+function Collide(x1,y1,w1,h1, x2,y2,w2,h2)
   return x1 < x2+w2 and
          x2 < x1+w1 and
          y1 < y2+h2 and
@@ -17,26 +17,46 @@ function Ball:Draw()
   love.graphics.rectangle("fill", self.x, self.y, self.edge, self.edge)
 end
 
-function Ball:Update(dt, playerX, playerY)
-  --[[if CheckCollision(self.x, self.y, self.edge, self.edge, playerX, playerY, 10, 60) then
-    self.x = self.x - (self.speed * dt)
-  elseif self.x - self.edge >= love.graphics.getWidth() then
-    self.x = self.x + (self.speed * dt)
-  end]]
-
+function Ball:Update(dt)
     self.x = self.x + (self.velX * dt)
     self.y = self.y + (self.velY * dt)
-
-
-if CheckCollision(self.x, self.y, self.edge, self.edge, playerX, playerY, 10, 60) then
-  self.velX = 230
-  self.velY = 30
 end
 
-if self.x >= love.graphics.getWidth() then
-  self.velX = -230
-end
+function Ball:CheckCollision(playerX, playerY)
+  -- Ball & paddle TODO: Add Y movement
+  if Collide(self.x, self.y, self.edge, self.edge, playerX, playerY, 10, 60) then
+    local intersectPc = self.x - playerY
+    angleMultiplier, degToRad =  math.abs(intersectPc / 30), (math.pi*2)/360
 
+     xV = math.cos(90 * angleMultiplier * degToRad) * 230
+     yV = math.sin(90 * angleMultiplier * degToRad) * 160
+
+    if intersectPc < 0 then
+      yV = 0 - yV
+    end
+
+    if (self.velX > 0) then
+      xV = 0 - xV
+    end
+
+    self.velX = xV
+    self.velY = yV
+  end
+
+  -- Ball and right wall
+  if self.x >= love.graphics.getWidth() then
+    self.velX = -230
+  end
+
+  -- Ball and top wall
+  if self.y <= 0 then
+    self.velY = 50
+  end
+
+  -- Ball and bottom wall
+  if self.y >= love.graphics.getHeight() - self.edge then
+    self.velY = -50
+  end
 end
 
 return Ball
--- main.lua
+++ main.lua
@@ -9,18 +9,27 @@ function love.load(arg)
 end
 
 function love.update(dt)
+  ball:CheckCollision(player.x, player.y)
+  ball:Update(dt)
 
-  ball:Update(dt, player.x, player.y)
 
   if love.keyboard.isDown('w') then
     player:Move("up", dt)
   elseif love.keyboard.isDown("s") then
     player:Move("down", dt)
+  elseif love.keyboard.isDown('escape') then
+    love.event.quit()
   end
 end
 
 function love.draw()
-  love.graphics.print("Collide: " .. tostring(collision), 0, 0)
+  if ball.xV ~= nil then
+    love.graphics.print("xV: " .. ball.xV, 0, 0)
+    love.graphics.print("yV: " .. ball.yV, 80, 0)
+  end
+  love.graphics.print("X Velocity: " .. ball.velX, 0, 40)
+  love.graphics.print("Y Velocity: " .. ball.velY, 80, 80)
+
   player:Draw()
   ball:Draw()
 end
